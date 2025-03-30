4f41bb4630f9d016bfb030dc609f74c70e794b0a
Aditya Das <adas0262@gmail.com>
2025-03-31 02:51:12 +0530

    setting up combobox

--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
+import { countryCodeList } from 'c/countryCodeList';
 
-export default class CurrencyConverter extends LightningElement {}
+export default class CurrencyConverter extends LightningElement {
+    countryFrom = 'USD';
+    countryTo = 'AUD';
+    countryList = countryCodeList;
+    handleChange(event) {
+        event.preventDefault();
+        const { name, value} = event.target
+        this[name] = value;
+    }
+}
