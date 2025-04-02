d566076111c9be32bcb8207445bbf7961d21d06d
Aditya Das <adas0262@gmail.com>
2025-04-03 02:25:56 +0530

    Exchange Rate api integration

--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -12,4 +12,21 @@ export default class CurrencyConverter extends LightningElement {
         const { name, value} = event.target
         this[name] = value;
     }
+
+    submitHandler(event) {
+        event.preventDefault();
+        this.convert();
+
+    }
+
+    async convert() {
+        const API_URL = `https://v6.exchangerate-api.com/v6/414fffa578df77c03e837139/pair/${this.countryFrom}/${this.countryTo}`;    
+        try {
+            const data = await fetch(API_URL);
+            const jsonData = await data.json();
+            console.log(jsonData)
+        } catch(error) {
+            console.log('Error:', error);
+        }
+    }
 }
