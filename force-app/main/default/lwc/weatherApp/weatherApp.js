af6827b6aca63438c0c6412211b30926b4000fe0
Aditya Das <adas0262@gmail.com>
2025-04-09 02:02:42 +0530

    Form Handling

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class WeatherApp extends LightningElement {}
+export default class WeatherApp extends LightningElement {
+    cityName = ''
+    searchHandler(event) {
+        this.cityName = event.target.value;
+    }
+
+    submitHandler(event) {
+        event.preventDefault(); // Prevent the default form submission
+        this.fetchData();
+    }
+
+    fetchData() {
+        console.log('Fetching data for city:', this.cityName);
+        // Fetch data from the weather API using the city name
+    }
+}
